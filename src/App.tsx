ac7ee237557eedef61aa0bfb1e7688dc1fc6b863
Pelonchas <pelonchas@192.168.1.10>
2025-07-24 15:54:11 -0700

    Added navbar icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <Grid
@@ -8,12 +8,15 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar></NavBar>
       </GridItem>
 
-      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
-        Aside
+      <GridItem 
+        area="aside" 
+        bg="gold" 
+        display={{ base: "none", lg: "block" }}>
+          Aside
       </GridItem>
 
       <GridItem area="main" bg="dodgerblue">
