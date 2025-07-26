a53a4d402294fa9d7e35bb0f2ce85e470c8f5c01
Pelonchas <pelonchas@192.168.1.6>
2025-07-26 02:28:05 -0700

    Updated assets, added description cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Box } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import MainImage from "./components/MainImage";
+import ServiceCards from "./components/ServiceCards";
 
 function App() {
   return (
     <Grid
+      maxWidth="1600px"
+      width="100%"
+      margin="0 auto"
       templateAreas={{
         base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        lg: `"nav" "main"`,
       }}
     >
-      <GridItem area="nav" bg="white">
+      <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <GridItem area="aside" bg="green" display={{base:"none", lg:"block"}}>
-        Aside
-      </GridItem>
-      <GridItem area="main" bg="yellow">
-        Main
+      <GridItem area="main" width="100%">
+        <Box height="88vh">
+          <Box bg="black" borderRadius="20px">
+            <MainImage></MainImage>
+          </Box>
+        </Box>
+        <ServiceCards></ServiceCards>
       </GridItem>
     </Grid>
   );
